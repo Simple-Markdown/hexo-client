7ae164f868dda4c241ec3fc24d8367660e66bf5d
microDreamSheep <atthemc313128460@gmail.com>
2022-11-30 11:00:56 +0800

    初始化

---
+++ src/main/java/live/midreamsheep/hexo/client/HexoClientStarter.java
@@ -0,0 +1,11 @@
+package live.midreamsheep.hexo.client;
+
+/**
+ * @author midreamsheep
+ * 启动器
+ * */
+public class HexoClientStarter {
+    public static void main(String[] args) {
+        System.out.println("初始化");
+    }
+}

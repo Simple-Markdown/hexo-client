4cb81945019a2e3b1b9f2c308ceb169c7b7cf5ee
microDreamSheep <atthemc313128460@gmail.com>
2022-11-30 21:40:28 +0800

    准备配置文件读取

--- src/main/java/live/midreamsheep/hexo/client/HexoClientStarter.java
+++ src/main/java/live/midreamsheep/hexo/client/HexoClientStarter.java
@@ -1,6 +1,10 @@
 package live.midreamsheep.hexo.client;
 
 import live.midreamsheep.command.ApplicationStarter;
+import live.midreamsheep.hexo.client.config.Config;
+import live.midreamsheep.hexo.client.tool.io.SIO;
+
+import java.io.File;
 
 /**
  * @author midreamsheep
@@ -9,6 +13,7 @@ import live.midreamsheep.command.ApplicationStarter;
 public class HexoClientStarter {
     public static void main(String[] args) {
         //配置文件读取
+        Config.setConfig(SIO.read(System.getProperty("user.dir") + "\\config.properties"));
         //连接建立
         //命令注入
         //命令行启动

f032d4e81e54e3c5c7e5e5441b90e53eb2554601
microDreamSheep <atthemc313128460@gmail.com>
2022-11-30 16:11:41 +0800

    准备

--- src/main/java/live/midreamsheep/hexo/client/HexoClientStarter.java
+++ src/main/java/live/midreamsheep/hexo/client/HexoClientStarter.java
@@ -1,11 +1,17 @@
 package live.midreamsheep.hexo.client;
 
+import live.midreamsheep.command.ApplicationStarter;
+
 /**
  * @author midreamsheep
  * 启动器
  * */
 public class HexoClientStarter {
     public static void main(String[] args) {
-        System.out.println("初始化");
+        //配置文件读取
+        //连接建立
+        //命令注入
+        //命令行启动
+        ApplicationStarter.main(args);
     }
 }

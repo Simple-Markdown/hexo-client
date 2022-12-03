0780ab5cc2e238effe6e171e90c57f6047b5ba20
microDreamSheep <atthemc313128460@gmail.com>
2022-12-03 21:28:04 +0800

    准备实时预览实现

---
+++ src/main/java/live/midreamsheep/hexo/client/hand/handlers/Server.java
@@ -0,0 +1,10 @@
+package live.midreamsheep.hexo.client.hand.handlers;
+
+import live.midreamsheep.hexo.client.hand.HandlerInter;
+
+public class Server implements HandlerInter {
+    @Override
+    public void handle(byte[] data) {
+        //启动实时预览功能
+    }
+}
---
+++ src/main/java/live/midreamsheep/hexo/client/hand/handlers/ServerStop.java
@@ -0,0 +1,10 @@
+package live.midreamsheep.hexo.client.hand.handlers;
+
+import live.midreamsheep.hexo.client.hand.HandlerInter;
+
+public class ServerStop implements HandlerInter {
+    @Override
+    public void handle(byte[] data) {
+        //停止实时预览功能
+    }
+}
